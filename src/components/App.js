5642afc2462469d13310d99c791a10c5be9da21d
jlmack13 <jennimacklin@gmail.com>
2018-03-28 13:44:03 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,45 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            fruit: [],
+            filters: [],
+            currentFilter: null
+        }
+    }
+
+    componentDidMount() {
+        this.fetchFruitList();
+    }
+
+    fetchFruitList = () => {
+        fetch('/api/fruit')
+            .then(response => response.json())
+            .then(items => this.setState({ items }));
+    }
+
+    componentWillMount() {
+        this.fetchFilters();
+    }
+
+    fetchFilters = () => {
+        fetch('/api/fruit_types')
+            .then(response => response.json())
+            .then(filters => this.setState({ filters }));
+    }
+
+    handleFilterChange = (event) => {
+        this.setState({currentFilter: event.target.value});
+    }
+
+    render() {
+        return(
+            <FruitBasket />
+        )
+    }
+}
 
 export default App;
--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -3,30 +3,22 @@ import React, { Component }  from 'react';
 import Filter from './Filter';
 import FilteredFruitList from './FilteredFruitList.js';
 
-class FruitBasket extends Component {
-  constructor() {
-    super();
+const FruitBasket = (props) => {
 
-    this.state = {
-      filters: [],
-      selectedFilter: null
-    };
-  }
+  return (
+    <div className="fruit-basket">
+      <Filter handleChange={props.handleFilterChange} />
+      <FilteredFruitList
+        filter={props.currentFilter} />
+    </div>
+  );
+}
 
-  handleFilterChange = event => {
-    console.log('new filter: ', event.target.value);
-    this.setState({ selectedFilter: event.target.value });
-  }
-
-  render() {
-    return (
-      <div className="fruit-basket">
-        <Filter handleChange={this.handleFilterChange} />
-        <FilteredFruitList
-          filter={this.state.selectedFilter} />
-      </div>
-    );
-  }
+FruitBasket.defaultProps = {
+  currentFilter: null,
+  updateFilterCallback: () => {},
+  fruit: [],
+  filters: []
 }
 
 export default FruitBasket;
